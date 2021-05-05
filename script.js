c3ff9a8ee36710bfc617b9d6d28db81e5f2b38a7
kou829 <73116796+kou829@users.noreply.github.com>
2021-05-05 13:37:45 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// トップ画像のスライド
+const img_src = ["image/コンパス 画像８.jpg","image/コンパス 画像１.jpg","image/コンパス 画像９.jpg"];
+      let num = -1;
+
+      function slide_time() {
+        if (num === 2) {
+          num = 0;
+        } else {
+          num++;
+        }
+        document.getElementById("slide_img").src = img_src[num];
+      }
+
+      setInterval(slide_time, 3000);
+
+
+// アニメーション　フワッと表示
+const boxElement = document.querySelectorAll(".box");
+console.log("画面の高さ", window.innerHeight)
+document.addEventListener("scroll", function(){
+    for (let i = 0; i < boxElement.length; i++) {
+        const getElementDistance = boxElement[i].getBoundingClientRect().top + boxElement[i].clientHeight * 0.6
+        if (window.innerHeight > getElementDistance){
+            boxElement[i].classList.add("show");
+        }
+    }
+})
+
+// スワイパーのCSS
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+
+    autoplay: {
+        delay: 3000
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
